ce0934f75ea6558b6ce35045b729d2db80b2694f
Asad272002 <asadnadeem6799@gmail.com>
2026-07-02 14:24:43 +0500

    Collaborative Code env setup with frontE and BackE

---
+++ Frontend/src/app/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css"
+import { Editor } from "@monaco-editor/react"
+import {MonacoBinding} from "y-monaco"
+
+
+
+function App() {
+
+  return (
+    <main className="h-screen w-full bg-gray-800 flex gap-4 p-4">
+
+      <aside className="h-full w-1/4 bg-amber-50 rounded-lg"></aside>
+      <section className="w-3/4 bg-neutral-800 rounded-lg overflow-hidden">
+      <Editor 
+      height= "100%"
+      defaultLanguage="javascript"
+      defaultValue="// some comment"
+      theme="vs-dark" 
+      />
+      </section>
+
+    </main>
+  )
+}
+
+export default App
+0
