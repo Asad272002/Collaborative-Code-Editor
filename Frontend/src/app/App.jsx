ca5eaf74c6d917b5c4ec2a1173791209c4663f56
Asad272002 <asadnadeem6799@gmail.com>
2026-07-02 14:57:07 +0500

    working socket layer without CRDT

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,10 +1,35 @@
 import "./App.css"
 import { Editor } from "@monaco-editor/react"
+import { useMemo, useRef } from "react"
 import {MonacoBinding} from "y-monaco"
+import * as Y from "yjs"
+import {SocketIOProvider} from "y-socket.io"
 
 
 
 function App() {
+
+  const editorRef = useRef(null)
+
+  const ydoc = useMemo(()=> new Y.Doc(), [])
+  const yText = useMemo(() => ydoc.getText("monaco"), [ydoc])
+
+
+  const handleMount = (editor) => {
+    
+    editorRef.current = editor
+
+    const provider = new SocketIOProvider("http://localhost:3000", "monaco", ydoc, {
+      autoConnect: true, 
+    })
+    const monacoBinding = new MonacoBinding(
+      yText, 
+      editorRef.current.getModel(),
+      new Set ([editorRef.current]),
+      provider.awareness
+    )
+
+  }
 
   return (
     <main className="h-screen w-full bg-gray-800 flex gap-4 p-4">
@@ -16,6 +41,7 @@ function App() {
       defaultLanguage="javascript"
       defaultValue="// some comment"
       theme="vs-dark" 
+      onMount={handleMount}
       />
       </section>
 
